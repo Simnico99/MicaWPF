fd3109d5c419c7307a8ecfabb7bb164bc2ffe55e
Simnico <Simnico99@hotmail.com>
2021-10-29 08:00:41 -0400

    Initial Upload.

---
+++ MicaWPF/Helpers/MicaHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Interop;
+using System.Windows;
+
+namespace MicaWPF.Helpers
+{
+    public static class MicaHelper
+    {
+        [DllImport("dwmapi.dll")]
+        private static extern int DwmSetWindowAttribute(IntPtr hwnd, DwmWindowAttribute dwAttribute, ref int pvAttribute, int cbAttribute);
+
+        [Flags]
+        private enum DwmWindowAttribute : uint
+        {
+            DWMWA_USE_IMMERSIVE_DARK_MODE = 20,
+            DWMWA_MICA_EFFECT = 1029
+        }
+
+        public static void UpdateStyleAttributes(this Window window)
+        {
+            IntPtr windowHandle = new WindowInteropHelper(window).Handle;
+            var darkThemeEnabled = ThemeHelper.GetWindowsTheme();
+
+            int trueValue = 0x01;
+            int falseValue = 0x00;
+
+            _ = darkThemeEnabled == WindowsTheme.Dark
+                ? DwmSetWindowAttribute(windowHandle, DwmWindowAttribute.DWMWA_USE_IMMERSIVE_DARK_MODE, ref trueValue, Marshal.SizeOf(typeof(int)))
+                : DwmSetWindowAttribute(windowHandle, DwmWindowAttribute.DWMWA_USE_IMMERSIVE_DARK_MODE, ref falseValue, Marshal.SizeOf(typeof(int)));
+
+            _ = DwmSetWindowAttribute(windowHandle, DwmWindowAttribute.DWMWA_MICA_EFFECT, ref trueValue, Marshal.SizeOf(typeof(int)));
+        }
+    }
+}
---
+++ MicaWPF/Helpers/ThemeHelper.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicaWPF.Helpers
+{
+    public enum WindowsTheme
+    {
+        Light,
+        Dark
+    }
+
+    public class ThemeHelper
+    {
+        private const string _registryKeyPath = @"Software\Microsoft\Windows\CurrentVersion\Themes\Personalize";
+
+        private const string _registryValueName = "AppsUseLightTheme";
+
+
+        public static WindowsTheme GetWindowsTheme()
+        {
+
+            using RegistryKey key = Registry.CurrentUser.OpenSubKey(_registryKeyPath);
+            object registryValueObject = key?.GetValue(_registryValueName);
+            if (registryValueObject == null)
+            {
+                return WindowsTheme.Light;
+            }
+
+            int registryValue = (int)registryValueObject;
+
+            return registryValue > 0 ? WindowsTheme.Light : WindowsTheme.Dark;
+        }
+    }
+}
