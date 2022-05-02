1191f48ff24f40658e5b63e4906b1f33168ebcc5
Nicolas Simoneau <simnico99@hotmail.com>
2022-05-01 21:24:48 -0400

    Moved to src folder and added Demo app

---
+++ src/MicaWPF.Demo/MicaWPF.Demo/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿using MicaWPF.Controls;
+
+namespace MicaWPF.Demo;
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : MicaWindow
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
