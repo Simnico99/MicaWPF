645f077f993ea8aaf63090d32ec7bcce70e979e6
Nicolas Simoneau <simnico99@hotmail.com>
2022-05-12 18:58:39 -0400

    Modified TitleBarContent to allow for right items. + Added a Independency Injection

---
+++ src/MicaWPF.DependencyInjection/Helpers/DependencyInjectionHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MicaWPF.DependencyInjection.Services;
+using MicaWPF.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace MicaWPF.DependencyInjection.Helpers;
+public static class DependencyInjectionHelper
+{
+    public static IHostBuilder UseMicaWPF(this IHostBuilder builder)
+    {
+        if (builder is null)
+        {
+            throw new ArgumentNullException(nameof(builder));
+        }
+
+        builder.ConfigureServices((_, collection) => 
+        {
+            collection.AddTransient<IThemeService, ThemeServiceDI>();
+            collection.AddTransient<IAccentColorService, AccentColorServiceDI>();
+        });
+
+        return builder;
+    }
+}
---
+++ src/MicaWPF.DependencyInjection/Services/AccentColorServiceDI.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using MicaWPF.Services;
+
+namespace MicaWPF.DependencyInjection.Services;
+internal class AccentColorServiceDI : IAccentColorService
+{
+    private readonly AccentColorService _localThemeService = AccentColorService.GetCurrent();
+
+    public bool AccentUpdateFromWindows => _localThemeService.AccentUpdateFromWindows;
+
+    public Color SystemAccentColor => _localThemeService.SystemAccentColor;
+
+    public Color SystemAccentColorDark1 => _localThemeService.SystemAccentColorDark1;
+
+    public Color SystemAccentColorDark2 => _localThemeService.SystemAccentColorDark2;
+
+    public Color SystemAccentColorDark3 => _localThemeService.SystemAccentColorDark3;
+
+    public Color SystemAccentColorLight1 => _localThemeService.SystemAccentColorLight1;
+
+    public Color SystemAccentColorLight2 => _localThemeService.SystemAccentColorLight2;
+
+    public Color SystemAccentColorLight3 => _localThemeService.SystemAccentColorLight3;
+
+    public void UpdateAccents(Color systemAccent)
+    {
+        _localThemeService.UpdateAccents(systemAccent);
+    }
+
+    public void UpdateAccentsFromWindows()
+    {
+        _localThemeService.UpdateAccentsFromWindows();
+    }
+}
