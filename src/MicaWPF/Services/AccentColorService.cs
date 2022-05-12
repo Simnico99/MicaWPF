53d5191ff1cfd15a8c9c7b208d7e3f7e40b835a8
Nicolas Simoneau <simnico99@hotmail.com>
2022-05-12 15:30:30 -0400

    Fixed a merge failure

--- src/MicaWPF/Services/AccentColorService.cs
+++ src/MicaWPF/Services/AccentColorService.cs
@@ -1,12 +1,11 @@
 ﻿using MicaWPF.Extensions;
 using MicaWPFRuntimeComponent;
-
 namespace MicaWPF.Services;
-
-public class AccentColorService : IAccentColorService
+public class AccentColorService
 {
     private static readonly AccentColorService _systemColorsHandler = new();
-    public bool AccentUpdateFromWindows { get; private set; }
+    private bool _isInit = false;
+    public bool AccentUpdateFromWindows { get; private set; } = true;
 
     public Color SystemAccentColor { get; private set; }
     public Color SystemAccentColorLight1 { get; private set; }
@@ -16,56 +15,60 @@ public class AccentColorService : IAccentColorService
     public Color SystemAccentColorDark2 { get; private set; }
     public Color SystemAccentColorDark3 { get; private set; }
 
+    internal void Init()
+    {
+        if (!_isInit)
+        {
+            _isInit = true;
+            if (AccentUpdateFromWindows)
+            {
+                UpdateAccentsFromWindows();
+                return;
+            }
+        }
+    }
+
     private AccentColorService() { }
 
     private void SetColorProperty(string propertyName, object value)
     {
         var type = GetType();
         var propertyInfo = type.GetProperty(propertyName);
-
         propertyInfo?.SetValue(this, Convert.ChangeType(value, propertyInfo.PropertyType), null);
     }
 
     private static Color GetOffSet(double hue, double hueCoefficient, double saturation, double value, double valueCoefficient, WindowsTheme windowsTheme)
     {
-        if (windowsTheme is WindowsTheme.Auto)
-        {
-            windowsTheme = ThemeHelper.GetWindowsTheme();
-        }
-
         return windowsTheme switch
         {
             WindowsTheme.Dark => HSVColorHelper.RGBFromHSV(Math.Min(hue + hueCoefficient, 360), saturation, Math.Min(value + valueCoefficient, 1)),
-            WindowsTheme.Light => HSVColorHelper.RGBFromHSV(Math.Min(hue + hueCoefficient, 360), saturation, Math.Min(value - valueCoefficient, 1)),
+            WindowsTheme.Light => HSVColorHelper.RGBFromHSV(Math.Min(hue + hueCoefficient, 360), saturation, Math.Min(value - valueCoefficient / 2, 1)),
             _ => throw new ArgumentOutOfRangeException(nameof(windowsTheme)),
         };
     }
 
-    private static Color GetThemeColorVariation((double hue, double saturation, double value) hsv, WindowsTheme theme, AccentBrushType accentBrushType)
+    private static Color GetThemeColorVariation((double hue, double saturation, double value) hsv, WindowsTheme windowsTheme, AccentBrushType accentBrushType)
     {
         var hueCoefficient = 0;
         if (1 - hsv.value < 0.15)
         {
             hueCoefficient = 1;
         }
-
         return accentBrushType switch
         {
             AccentBrushType.Primary => HSVColorHelper.RGBFromHSV(hsv.hue, hsv.saturation, hsv.value),
-            AccentBrushType.Secondary => GetOffSet(hsv.hue, hueCoefficient, hsv.saturation, hsv.value, 0.3, theme),
-            AccentBrushType.Tertiary => GetOffSet(hsv.hue, hueCoefficient, hsv.saturation, hsv.value, 0.35, theme),
-            AccentBrushType.Quaternary => GetOffSet(hsv.hue, hueCoefficient, hsv.saturation, hsv.value, 0.65, theme),
+            AccentBrushType.Secondary => GetOffSet(hsv.hue, hueCoefficient, hsv.saturation, hsv.value, 0.3, windowsTheme),
+            AccentBrushType.Tertiary => GetOffSet(hsv.hue, hueCoefficient, hsv.saturation, hsv.value, 0.35, windowsTheme),
+            AccentBrushType.Quaternary => GetOffSet(hsv.hue, hueCoefficient, hsv.saturation, hsv.value, 0.65, windowsTheme),
             _ => throw new ArgumentOutOfRangeException(nameof(accentBrushType)),
         };
     }
-
     private static void UpdateColorResources(Color systemAccent, Color primaryAccent, Color secondaryAccent, Color tertiaryAccent)
     {
         Application.Current.Resources["MicaWPF.Colors.SystemAccentColor"] = systemAccent;
         Application.Current.Resources["MicaWPF.Colors.SystemAccentColorLight1"] = primaryAccent;
         Application.Current.Resources["MicaWPF.Colors.SystemAccentColorLight2"] = secondaryAccent;
         Application.Current.Resources["MicaWPF.Colors.SystemAccentColorLight3"] = tertiaryAccent;
-
         Application.Current.Resources["MicaWPF.Brushes.SystemAccent"] = secondaryAccent.ToBrush();
         Application.Current.Resources["MicaWPF.Brushes.SystemFillColorAttention"] = secondaryAccent.ToBrush();
         Application.Current.Resources["MicaWPF.Brushes.AccentTextFillColorPrimary"] = tertiaryAccent.ToBrush();
@@ -73,19 +76,13 @@ public class AccentColorService : IAccentColorService
         Application.Current.Resources["MicaWPF.Brushes.AccentTextFillColorTertiary"] = secondaryAccent.ToBrush();
         Application.Current.Resources["MicaWPF.Brushes.AccentFillColorSelectedTextBackground"] = systemAccent.ToBrush();
         Application.Current.Resources["MicaWPF.Brushes.AccentFillColorDefault"] = secondaryAccent.ToBrush();
-
         Application.Current.Resources["MicaWPF.Brushes.AccentFillColorSecondary"] = secondaryAccent.ToBrush(0.9);
         Application.Current.Resources["MicaWPF.Brushes.AccentFillColorTertiary"] = secondaryAccent.ToBrush(0.8);
     }
 
-    private void UpdateFromInternalColors(WindowsTheme windowsTheme)
+    private void UpdateFromInternalColors()
     {
-        if (windowsTheme is WindowsTheme.Auto)
-        {
-            windowsTheme = ThemeHelper.GetWindowsTheme();
-        }
-
-        switch (windowsTheme)
+        switch (ThemeService.GetCurrent().CurrentTheme)
         {
             case WindowsTheme.Dark:
                 UpdateColorResources(SystemAccentColor, SystemAccentColorLight1, SystemAccentColorLight2, SystemAccentColorLight3);
@@ -96,9 +93,11 @@ public class AccentColorService : IAccentColorService
         }
     }
 
-    public void UpdateAccentsFromWindows(WindowsTheme windowsTheme = WindowsTheme.Light)
+    public void UpdateAccentsFromWindows()
     {
+        _isInit = true;
         AccentUpdateFromWindows = true;
+
         var uwpColors = new UWPColors();
         var colorsLongString = uwpColors.GetSystemColors();
 
@@ -112,8 +111,9 @@ public class AccentColorService : IAccentColorService
         UpdateFromInternalColors();
     }
 
-    public void SetAccents(Color systemAccent, WindowsTheme windowsTheme = WindowsTheme.Light)
+    public void UpdateAccents(Color systemAccent)
     {
+        _isInit = true;
         AccentUpdateFromWindows = false;
         SystemAccentColor = systemAccent;
         SystemAccentColorLight1 = GetThemeColorVariation(HSVColorHelper.ConvertToHSVColor(System.Drawing.Color.FromArgb(systemAccent.R, systemAccent.G, systemAccent.B)), WindowsTheme.Dark, AccentBrushType.Secondary);
@@ -123,7 +123,7 @@ public class AccentColorService : IAccentColorService
         SystemAccentColorDark2 = GetThemeColorVariation(HSVColorHelper.ConvertToHSVColor(System.Drawing.Color.FromArgb(systemAccent.R, systemAccent.G, systemAccent.B)), WindowsTheme.Light, AccentBrushType.Tertiary);
         SystemAccentColorDark3 = GetThemeColorVariation(HSVColorHelper.ConvertToHSVColor(System.Drawing.Color.FromArgb(systemAccent.R, systemAccent.G, systemAccent.B)), WindowsTheme.Light, AccentBrushType.Quaternary);
 
-        UpdateFromInternalColors(windowsTheme);
+        UpdateFromInternalColors();
     }
 
     public static AccentColorService GetCurrent()
