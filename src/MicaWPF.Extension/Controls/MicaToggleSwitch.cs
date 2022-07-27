7f6e9e108b0fb9d811bb84f92b1825e3ed14aa2f
Simnico99 <Simnico99@hotmail.com>
2022-07-27 10:37:30 -0400

    Added ToggleSwitches

---
+++ src/MicaWPF.Extension/Controls/MicaToggleSwitch.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls.Primitives;
+
+namespace MicaWPF.Extension.Controls;
+public class MicaToggleSwitch : ToggleButton
+{
+}
