28a23cde70e6e73a3dc5f04692678d1a4ecf1956
Simnico99 <Simnico99@hotmail.com>
2022-07-28 07:30:22 -0400

    Added Listview

---
+++ src/MicaWPF/Converters/BrushToColorConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicaWPF.Converters;
+internal class BrushToColorConverter
+{
+    public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+    {
+        if (value is SolidColorBrush brush)
+            return brush.Color;
+
+        if (value is Color)
+            return value;
+
+        // We draw red to visibly see an invalid bind in the UI.
+        return new Color { A = 255, R = 255, G = 0, B = 0 };
+    }
+
+    /// <summary>
+    /// Not Implemented.
+    /// </summary>
+    /// <exception cref="NotImplementedException"></exception>
+    public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
