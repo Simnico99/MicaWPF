9fe8c76cea02b40c0033e0a47f4fed7b737237a3
Simnico99 <Simnico99@hotmail.com>
2022-07-29 12:21:42 -0400

    Made the progress rings looks more like Windows original ones.

--- src/MicaWPF/Converters/ProgressThicknessConverter.cs
+++ src/MicaWPF/Converters/ProgressThicknessConverter.cs
@@ -5,7 +5,7 @@ public class ProgressThicknessConverter : IValueConverter
 {
     public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
     {
-        return value is double height ? height / 8 : (object)12.0d;
+        return value is double height ? height / 11 : (object)12.0d;
     }
 
     public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
