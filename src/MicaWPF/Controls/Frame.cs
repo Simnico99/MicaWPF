e77681aa75a40ab0e1ce98627b42cb80935bf330
Simnico99 <Simnico99@hotmail.com>
2022-08-18 09:44:38 -0400

    Added Frame to refresh controls if the style change then you navigate.

---
+++ src/MicaWPF/Controls/Frame.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MicaWPF.Extensions;
+
+namespace MicaWPF.Controls;
+public class Frame : System.Windows.Controls.Frame
+{
+    public Frame()
+    {
+        Navigated += Frame_Navigated;
+    }
+
+    private void Frame_Navigated(object sender, System.Windows.Navigation.NavigationEventArgs e)
+    {
+        foreach (var element in this.FindVisualChildrens<FrameworkElement>())
+        {
+            var savedStyle = element.Style;
+            element.Style = null;
+
+            element.UpdateDefaultStyle();
+
+            element.Style = savedStyle;
+        }
+    }
+}
