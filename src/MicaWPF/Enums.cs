3897f2ba47e794b30d5595c77ddeeb47c8f70e7e
Simnico99 <Simnico99@hotmail.com>
2022-09-13 11:16:08 -0400

    Removed useseless enum

--- src/MicaWPF/Enums.cs
+++ src/MicaWPF/Enums.cs
@@ -47,18 +47,3 @@ public enum TitleBarType
     Win32,
     WinUI
 }
-
-[Flags]
-internal enum Facility
-{
-    Null,
-    Rpc,
-    Dispatch,
-    Storage,
-    Itf,
-    Win32 = 7,
-    Windows,
-    Control = 10,
-    Ese = 3678,
-    WinCodec = 2200
-}
