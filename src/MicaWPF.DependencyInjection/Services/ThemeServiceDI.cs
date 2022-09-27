65749ec07e19eb265ad11878566f8f764973d5c2
Simnico99 <Simnico99@hotmail.com>
2022-09-27 13:53:38 -0400

    Fixed the implementation issues + Modified targeted Frameworks.

--- src/MicaWPF.DependencyInjection/Services/ThemeServiceDI.cs
+++ src/MicaWPF.DependencyInjection/Services/ThemeServiceDI.cs
@@ -9,19 +9,15 @@ internal sealed class ThemeServiceDI : IThemeService
 {
     private readonly MicaWPFOptions _options;
     public IWeakEvent<WindowsTheme> ThemeChanged => ThemeService.Current.ThemeChanged;
+    public List<MicaEnabledWindow> MicaEnabledWindows => ThemeService.Current.MicaEnabledWindows;
+    public WindowsTheme CurrentTheme => ThemeService.Current.CurrentTheme;
+    public bool IsThemeAware => ThemeService.Current.IsThemeAware;
 
     public ThemeServiceDI(MicaWPFOptions options)
     {
         _options = options;
-        ThemeService.Current.IsThemeAware = _options.IsThemeAware;
         _ = ThemeService.Current.ChangeTheme(_options.Theme);
     }
-
-    public WindowsTheme CurrentTheme => ThemeService.Current.CurrentTheme;
-
-    public bool IsThemeAware { get => ThemeService.Current.IsThemeAware; set => ThemeService.Current.IsThemeAware = value; }
-
-    public ICollection<MicaEnabledWindow> MicaEnabledWindows => ThemeService.Current.MicaEnabledWindows;
 
     public WindowsTheme ChangeTheme(WindowsTheme windowsTheme = WindowsTheme.Auto)
     {
