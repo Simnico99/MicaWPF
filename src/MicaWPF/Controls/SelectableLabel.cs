31fa9c6ada2c46caa1046ae3db94fb185bed9a8a
Simnico99 <Simnico99@hotmail.com>
2023-01-19 08:53:40 -0500

    Added a selectable Label

---
+++ src/MicaWPF/Controls/SelectableLabel.cs
@@ -0,0 +1,12 @@
+﻿using MicaWPF.Symbols;
+using System.ComponentModel;
+
+namespace MicaWPF.Controls;
+
+/// <summary>
+/// A WinUI TextBox based on <see cref="System.Windows.Controls.TextBox"/>
+/// </summary>
+[ToolboxItem(true)]
+public class SelectableLabel : System.Windows.Controls.TextBox
+{
+}
