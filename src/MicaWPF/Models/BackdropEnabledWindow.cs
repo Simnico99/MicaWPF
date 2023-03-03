6569707b3860125bcdb81173cada46d6698f7f9f
Simnico99 <Simnico99@hotmail.com>
2023-03-03 13:46:21 -0500

    Added summary to BackdropEnabledWindow

--- src/MicaWPF/Models/BackdropEnabledWindow.cs
+++ src/MicaWPF/Models/BackdropEnabledWindow.cs
@@ -1,10 +1,26 @@
 ﻿namespace MicaWPF.Models;
-public readonly struct MicaEnabledWindow
+
+/// <summary>
+/// Represents a readonly struct that contains information about a window and its backdrop type.
+/// </summary>
+public readonly struct BackdropEnabledWindow
 {
+    /// <summary>
+    /// Gets the Window associated with this BackdropEnabledWindow instance.
+    /// </summary>
     public Window Window { get; }
+
+    /// <summary>
+    /// Gets the BackdropType associated with this BackdropEnabledWindow instance.
+    /// </summary>
     public BackdropType BackdropType { get; }
 
-    public MicaEnabledWindow(Window window, BackdropType backdropType)
+    /// <summary>
+    /// Initializes a new instance of the BackdropEnabledWindow class with the specified Window and BackdropType.
+    /// </summary>
+    /// <param name="window">The Window associated with this BackdropEnabledWindow instance.</param>
+    /// <param name="backdropType">The BackdropType associated with this BackdropEnabledWindow instance.</param>
+    public BackdropEnabledWindow(Window window, BackdropType backdropType)
     {
         Window = window;
         BackdropType = backdropType;
