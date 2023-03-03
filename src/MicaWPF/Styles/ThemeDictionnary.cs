1f4be5e27a0a23b9732a15bc1f954d15ad7f097d
Simnico99 <Simnico99@hotmail.com>
2023-03-03 08:19:19 -0500

    Modified the theme usage

---
+++ src/MicaWPF/Styles/ThemeDictionnary.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows.Markup;
+
+namespace MicaWPF.Styles;
+
+[Localizability(LocalizationCategory.Ignore)]
+[Ambient]
+[UsableDuringInitialization(true)]
+public sealed class ThemeDictionnary : ResourceDictionary
+{
+    public WindowsTheme Theme
+    {
+        set
+        {
+            var themeName = value switch
+            {
+                WindowsTheme.Dark => "MicaDark",
+                _ => "MicaLight"
+            };
+
+            //MergedDictionaries.Add(new ResourceDictionary() { Source = new($"pack://application:,,,/MicaWPF;component/Styles/Themes/{themeName}.xaml", UriKind.Absolute) });
+            Source = new($"pack://application:,,,/MicaWPF;component/Styles/Themes/{themeName}.xaml", UriKind.Absolute);
+        } 
+    }
+}
