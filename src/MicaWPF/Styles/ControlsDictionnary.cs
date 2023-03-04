82bea5c7ddb70f25f6e9c40c6ee19177082b2a69
Simnico99 <simnico99@hotmail.com>
2023-03-03 19:07:47 -0500

    Fixed issues with control colors

---
+++ src/MicaWPF/Styles/ControlsDictionnary.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Markup;
+
+namespace MicaWPF.Styles;
+
+[Localizability(LocalizationCategory.Ignore)]
+[Ambient]
+[UsableDuringInitialization(true)]
+public sealed class ControlsDictionnary : ResourceDictionary
+{
+    public ControlsDictionnary()
+    {
+        Source = new($"pack://application:,,,/MicaWPF;component/Styles/MicaWPF.xaml", UriKind.Absolute);
+    }
+}
