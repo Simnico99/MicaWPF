5ea4a125683ae7ff893f65ee45c6287987ae9f0d
Simnico99 <simnico99@hotmail.com>
2023-03-18 12:23:14 -0400

    Codefactor cleanup

--- src/MicaWPF/Controls/MicaWindow.cs
+++ src/MicaWPF/Controls/MicaWindow.cs
@@ -198,10 +198,6 @@ public class MicaWindow : Window
     private void OnMaximizeWindow(object target, ExecutedRoutedEventArgs e)
     {
         SystemCommands.MaximizeWindow(this);
-        //if (_buttonMax is not null)
-        //{
-        //    _buttonMax.Background = Brushes.Transparent;
-        //}
     }
 
     private void OnMinimizeWindow(object target, ExecutedRoutedEventArgs e)
@@ -242,7 +238,6 @@ public class MicaWindow : Window
             return PtrHelper.Create(_hTMAXBUTTON);
         }
 
-
         return PtrHelper.Zero;
     }
 
@@ -277,7 +272,6 @@ public class MicaWindow : Window
         {
             return PtrHelper.Zero;
         }
-
 
         switch (msg)
         {
