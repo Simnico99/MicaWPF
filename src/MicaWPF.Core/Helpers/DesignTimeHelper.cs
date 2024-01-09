44e73059cc9eca6ff40f7fc4d7bfe7fca1a62da0
Simnico99 <simnico99@hotmail.com>
2024-01-09 08:28:38 -0500

    Specifying theme is now optional + now design mode theme option.

---
+++ src/MicaWPF.Core/Helpers/DesignTimeHelper.cs
@@ -0,0 +1,13 @@
+﻿// <copyright file="DesignTimeHelper.cs" company="Zircon Technology">
+// This software is distributed under the MIT license and its code is open-source and free for use, modification, and distribution.
+// </copyright>
+
+using System.ComponentModel;
+using System.Windows;
+
+namespace MicaWPF.Core.Helpers;
+
+public static class DesignTimeHelper
+{
+    public static bool IsDesignMode { get; } = DesignerProperties.GetIsInDesignMode(new DependencyObject());
+}
